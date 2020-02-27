081316c5aef71c75794125acc8cce841dbcbd549
Ademir Balbuena <rojasademir735@gmail.com>
2020-02-26 21:05:13 -0300

    Mi primer Commit

---
+++ js/aplicacion.js
@@ -0,0 +1,15 @@
+
+
+
+/*let texto=$("#item_1").text();
+
+
+$("h3").text(texto);
+
+
+console.log("Llegue aqui")*/
+$(".hot").click(function () {
+	//console.log("funciona el click")
+	let texto=$(this).text();
+$("h3").text(texto);
+});
